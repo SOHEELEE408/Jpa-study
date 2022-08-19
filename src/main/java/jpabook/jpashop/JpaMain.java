16e532010bce4a9dbb609ceef337ca2664278fbd
SOHEELEE408 <dlwlsrl33@gmail.com>
2022-08-19 22:10:13 +0900

    pratice : 다양한 연관관계 매핑 연습(@ManyToMany, @OneToOne)

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,7 +1,5 @@
 package jpabook.jpashop;
 
-import com.sun.org.apache.xpath.internal.operations.Or;
-import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -9,7 +7,6 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.util.List;
 
 public class JpaMain {
 
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,12 +1,9 @@
 package jpabook.jpashop.domain;
 
-import com.sun.org.apache.xpath.internal.operations.Or;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Setter
